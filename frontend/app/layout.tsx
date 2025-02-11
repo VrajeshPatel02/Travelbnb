91ccf8c005b90aae89f346f002cdfdff4ec40018
aaravsoni1 <soniaarav596@gmail.com>
2025-02-11 22:24:13 +0530

    change in logo

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/airbnb-logo.svg" sizes="any" />
+        <link rel="icon" href="/house-logo.svg" type="image/svg+xml" sizes="32x32" />
       </head>
       <body>
         <AuthProvider>
--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -77,7 +77,7 @@ const LoginForm = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
-          src="/house-logo.svg"
+          src="/travelbnb-logo.svg"
           alt="travelbnb Logo"
           width={102}
           height={32}
